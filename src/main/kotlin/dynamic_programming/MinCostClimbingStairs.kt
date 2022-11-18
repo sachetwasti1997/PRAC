f48f1106fecd24b02a8579fbe19a505ffafce76f
sachetwasti1997 <sachetwasti61@gmail.com>
2022-11-18 22:07:48 +0530

    Code Push

--- src/main/kotlin/dynamic_programming/MinCostClimbingStairs.kt
+++ src/main/kotlin/dynamic_programming/MinCostClimbingStairs.kt
@@ -13,24 +13,25 @@ class MinCostClimbingStairs {
   }
    */
   fun findCost(cst: IntArray, n: Int, indx: Int, currCst: Int, dp: IntArray): Int {
-    if (indx >= n) return currCst
-    if (dp[indx] != Int.MAX_VALUE) return dp[indx]
-    dp[indx] = dp[indx].coerceAtMost(findCost(cst, n, indx + 1, currCst + cst[indx], dp).coerceAtMost(findCost(cst, n, indx + 2, currCst + cst[indx], dp)))
+    if (indx == n) return currCst
+    if (indx > n) return Int.MAX_VALUE
+    if (dp[indx] != -1) return dp[indx]
+    dp[indx] = Math.min(findCost(cst, n, indx + 1, currCst + cst[indx], dp),
+      findCost(cst, n, indx + 2, currCst + cst[indx], dp))
     return dp[indx]
   }
   fun minCostClimbingStairs(cost: IntArray): Int {
-    var dp = IntArray(cost.size+1){ Int.MAX_VALUE}
-    val f1 = findCost(cost, cost.size, 0, 0, dp)
+    var dp = IntArray(cost.size){-1}
+    val ans1 = findCost(cost, cost.size, 0, 0, dp)
     println(dp.contentToString())
-    dp = IntArray(cost.size+1){Int.MAX_VALUE}
-    val f2 = findCost(cost, cost.size, 1, 0, dp)
-    println("$f1 $f2")
+    dp = IntArray(cost.size){-1}
+    val ans2 = findCost(cost, cost.size, 1, 0, dp)
     println(dp.contentToString())
-    return 1
+    return ans1.coerceAtMost(ans2)
   }
 }
 
 fun main () {
   val m = MinCostClimbingStairs()
-  println(m.minCostClimbingStairs(intArrayOf(1,100,1,1,1,100,1,1,100,1)))
+  println(m.minCostClimbingStairs(intArrayOf(10,15,20)))
 }
